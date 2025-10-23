557222ee28845f5cb239c09ca76435f0ad988798
Kenny-Wouters <kendev007@outlook.com>
2025-10-23 15:17:21 +0200

    Add project files.

---
+++ WebAPICourse/Controllers/ShirtsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPICourse.Models;
+
+namespace WebAPICourse.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController : ControllerBase
+    {
+
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "List of shirts";
+        }
+
+        [HttpGet("{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Shirt with ID: {id}";
+        }
+
+        [HttpPost]
+        public string CreateShirt([FromForm] Shirt shirt)
+        {
+            return $"Created shirt with data: {shirt}";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id, string shirtData)
+        {
+            return $"Updated shirt with ID: {id} to data: {shirtData}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleted shirt with ID: {id}";
+        }
+
+
+    }
+
+}
